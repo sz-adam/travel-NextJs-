e48296a2d6c7bc86799126a60dd8c8cd7a24c414
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-30 09:35:10 +0200

    create card components

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,33 @@
+import React, { Dispatch, SetStateAction } from 'react'
+import { Travel } from '../types/travel'
+
+type Props = {
+  travel: Travel[]
+}
+
+const Card = ({ travel }: Props) => {
+  return (
+
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+      {travel?.map((items: Travel) => (
+        <div key={items.id} className="bg-white rounded-lg overflow-hidden w-auto border shadow-lg shadow-slate-600  hover:scale-105 duration-300">
+          <div>
+            <img src={items.images[0]} alt="car!" className="w-full h-64 object-cover" />
+          </div>
+          <div className="p-4">
+            <h2 className="text-2xl font-semibold">{items.location_name} ({items.city})</h2>
+            <p className="text-gray-600 mt-2">{items.description}</p>
+            <div className="mt-4 flex justify-between items-center">
+              <button type='submit' className="btn btn-outline btn-success">Megnéz</button>
+              <div className="text-sm text-gray-500 border px-3 py-1 rounded-full" >{items.category}</div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+
+  )
+}
+
+export default Card
+
