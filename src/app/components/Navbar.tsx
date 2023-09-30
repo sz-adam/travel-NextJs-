eabc865667e1494c97143e09aa6b9cfdf79266a6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-09-30 08:29:23 +0200

    create Navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar: React.FC = () => {
+ 
+    return (
+        <nav className="w-full bg-slate-200 h-16  flex items-center px-5">
+            <h2 className="text-2xl text-cyan-600 font-bold ">Coding Travel</h2>
+        </nav>
+    );
+}
+
+export default Navbar;
