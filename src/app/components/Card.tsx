3d34cc4a12a671886b0456a61104fde6fb153af7
sz-adam <112242221+sz-adam@users.noreply.github.com>
2023-10-21 22:30:33 +0200

    correction of card formatting

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -10,19 +10,19 @@ type Props = {
 const Card = ({ travel }: Props) => {
   return (
 
-    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 '>
       {travel?.map((items: Travel) => (
-        <div key={items.id} className="bg-white rounded-lg overflow-hidden w-auto border shadow-lg shadow-slate-600  hover:scale-105 duration-300">
+        <div key={items.id} className="bg-white rounded-2xl w-auto border shadow-lg shadow-slate-600 overflow-hidden hover:scale-105 duration-300 ">
           <div>
-            <img src={items.images[0]} alt="car!" className="w-full h-64 object-cover" />
+            <img src={items.images[0]} alt="car!" className="w-full h-64 object-cover rounded-t-2xl"  />
           </div>
-          <div className="p-4">
-            <h2 className="text-2xl font-semibold">{items.location_name} ({items.city})</h2>
-            <p className="text-gray-600 mt-2">{items.description}</p>
-            <div className="mt-4 flex justify-between items-center text-center">
+          <div className="p-4 ">
+            <h2 className="text-xl font-semibold">{items.location_name} ({items.city})</h2>
+            <p className="text-gray-600 mt-2 overflow-hidden text-ellipsis whitespace-nowrap">{items.description}</p>
+            <div className="mt-4 flex flex-wrap justify-between items-center text-center ">
               <Link href={`/travel/${items.location_name}`} className="btn">View</Link>
               <FullRating reviews={items.reviews} />
-              <div className="text-sm text-gray-500 border px-3 py-1 rounded-full" >{items.category}</div>
+              <div className="text-sm bg-slate-200 text-gray-500 border px-3 py-1 rounded-full" >{items.category}</div>
             </div>
           </div>
         </div>
--- src/app/components/DetailsCarousel.tsx
+++ src/app/components/DetailsCarousel.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
-import { RxDotFilled } from 'react-icons/rx'
 
 type Props = {
   images: string[];
